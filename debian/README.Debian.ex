1f3d77e6d91fad7dc00d214a60aafe68c5cf4500
Anudeep <f20212776@goa.bits-pilani.ac.in>
2025-03-26 19:12:02 +0000

    Remove unused build artifacts and example files in debian

--- debian/README.Debian.ex
+++
@@ -1,6 +0,0 @@
-python3-huggingface-hub for Debian
----------------------------------
-
-<Possible notes regarding this package - if none, delete this file.>
-
- -- anudeep <anudeep@unknown>  Mon, 24 Mar 2025 13:57:36 +0000
