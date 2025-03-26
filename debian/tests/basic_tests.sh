9a4707e48816a3e514d27a9e1358b6b03b3ff474
Anudeep <f20212776@goa.bits-pilani.ac.in>
2025-03-26 19:32:51 +0000

    Add autopkgtest for basic functionality

--- debian/tests/basic_tests.sh
+++ debian/tests/basic_tests.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -e
 
-if ! command -v huggingface-cli >/dev/null 2>&1;then
-   echo "hugging face cli not installed"
-   exit 1
-fi
-#test -f /usr/bin/huggingface-cli || {echo "huggingface cli not found";exit 1;}
-huggingface-cli --version || {echo "hugging face cli failed to run"; exit 1;}
+python3 -e "import huggingface_hub; print('huggingface_hub imported successfully')"
